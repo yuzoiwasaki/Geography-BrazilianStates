f464a8b3b9ef095a5448347b798fa9292aa6cf9d
yuzoiwasaki <a0556017@sophia.jp>
2015-06-27 16:53:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Geography::BrazilianStates - It's new $module
+
+# SYNOPSIS
+
+    use Geography::BrazilianStates;
+
+# DESCRIPTION
+
+Geography::BrazilianStates is ...
+
+# LICENSE
+
+Copyright (C) yuzoiwasaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+yuzoiwasaki <a0556017@sophia.jp>
---
+++ lib/Geography/BrazilianStates.pm
@@ -0,0 +1,39 @@
+package Geography::BrazilianStates;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Geography::BrazilianStates - It's new $module
+
+=head1 SYNOPSIS
+
+    use Geography::BrazilianStates;
+
+=head1 DESCRIPTION
+
+Geography::BrazilianStates is ...
+
+=head1 LICENSE
+
+Copyright (C) yuzoiwasaki.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+yuzoiwasaki E<lt>a0556017@sophia.jpE<gt>
+
+=cut
+
